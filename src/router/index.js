c0e71136c3be623bb13fbb2b29b179a8b82542e5
Marinario <ilbuonomosharro@gmail.com>
2023-07-16 16:47:47 +0200

    I've created a new directory called router with an index file that will hold the configuration for the router

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const routes = [
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
+
+export default router
