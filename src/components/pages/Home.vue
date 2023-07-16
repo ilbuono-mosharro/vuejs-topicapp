f97d1322b0da8c24dd14c5b525d649255c003769
Marinario <ilbuonomosharro@gmail.com>
2023-07-16 20:14:53 +0200

    I created a new component for the list of topics

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,16 +1,8 @@
 <script setup>
 import Base from "../Base.vue";
-import {useTopicsStore} from "../stores/topicsStore.js";
-import {onMounted} from "vue";
 import VueImage from '../../assets/vue.svg'
 import DjangoImage from '../../assets/django-logo-positive.svg'
-
-
-const topicStore = useTopicsStore()
-
-onMounted(async () => {
-  await topicStore.fetchTopics()
-})
+import Topics from "../topics/Topics.vue";
 </script>
 
 <template>
@@ -25,36 +17,7 @@ onMounted(async () => {
       <h1 class="h6 mb-0 text-white lh-1">Vue js</h1>
       </div>
   </div>
-  <div class="my-3 p-3 bg-body rounded shadow-sm">
-    <h6 class="border-bottom pb-2 mb-0">{{ topicStore.total }} Topics</h6>
-   <div class="table-responsive">
-     <table class="table table-borderless table-hover">
-       <thead>
-    <tr>
-      <th scope="col">Topic</th>
-      <th scope="col">Category</th>
-      <th scope="col"></th>
-      <th scope="col">Replies</th>
-      <th scope="col">Likes</th>
-      <th scope="col">Dislikes</th>
-      <th scope="col">Published</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="(topic, index) in topicStore.data" :key="index">
-      <th scope="row" class="fw-normal">{{topic.subject}}</th>
-      <th class="fw-normal">{{topic.category.name}}</th>
-      <th><img :src="topic.starter.avatar ? topic.starter.avatar : VueImage" class="img-fluid rounded-5" width="35" height="25" alt="" /></th>
-      <th class="fw-normal">2</th>
-      <th class="fw-normal">{{ topic.upvote_count }}</th>
-      <th class="fw-normal">{{ topic.downvote_count }}</th>
-      <th class="fw-normal">{{ topic.created_data }}</th>
-    </tr>
-  </tbody>
-
-     </table>
-   </div>
-  </div>
+  <Topics />
 </Base>
 </template>
 
---
+++ src/components/topics/Topics.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {useTopicsStore} from "../stores/topicsStore.js";
+import {onMounted} from "vue";
+import VueImage from '../../assets/vue.svg'
+
+
+const topicStore = useTopicsStore()
+
+onMounted(async () => {
+  await topicStore.fetchTopics()
+})
+</script>
+
+<template>
+<div class="my-3 p-3 bg-body rounded shadow-sm">
+    <h6 class="border-bottom pb-2 mb-0">{{ topicStore.total }} Topics</h6>
+   <div class="table-responsive">
+     <table class="table table-borderless table-hover">
+       <thead>
+    <tr>
+      <th scope="col">Topic</th>
+      <th scope="col">Category</th>
+      <th scope="col"></th>
+      <th scope="col">Replies</th>
+      <th scope="col">Likes</th>
+      <th scope="col">Dislikes</th>
+      <th scope="col">Published</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(topic, index) in topicStore.data" :key="index">
+      <th scope="row" class="fw-normal">{{topic.subject}}</th>
+      <th class="fw-normal">{{topic.category.name}}</th>
+      <th><img :src="topic.starter.avatar ? topic.starter.avatar : VueImage" class="img-fluid rounded-5" width="35" height="25" alt="" /></th>
+      <th class="fw-normal">2</th>
+      <th class="fw-normal">{{ topic.upvote_count }}</th>
+      <th class="fw-normal">{{ topic.downvote_count }}</th>
+      <th class="fw-normal">{{ topic.created_data }}</th>
+    </tr>
+  </tbody>
+     </table>
+   </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
