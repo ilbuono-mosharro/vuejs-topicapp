6b63fccb5d4bd60634044041aac4108b15d9b736
Marinario <ilbuonomosharro@gmail.com>
2023-07-16 16:45:05 +0200

    Created a pinia instance in src/main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+
+app.mount('#app')
