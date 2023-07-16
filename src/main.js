072115b25fa8b56a237e52f9f0e8d35251ebd6e9
Marinario <ilbuonomosharro@gmail.com>
2023-07-16 16:49:59 +0200

    I added the router in the main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
 import App from './App.vue'
+import router from "./router/index.js";
 import { createPinia } from 'pinia'
 
 const pinia = createPinia()
 const app = createApp(App)
 
+app.use(router)
 app.use(pinia)
 
 app.mount('#app')
