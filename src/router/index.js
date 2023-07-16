c0599f7c15c31f19be22b2f089054151c48afc7c
Marinario <ilbuonomosharro@gmail.com>
2023-07-16 20:09:44 +0200

    I created a new component for the topic detail and added the route in router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../components/pages/Home.vue";
+import Topic from "../components/topics/Topic.vue";
 
 const routes = [
   { path: '/', component: Home, name:"home" },
+  { path: '/topic/:id', component: Topic, name: "TopicDetail" },
 ]
 
 const router = createRouter({
