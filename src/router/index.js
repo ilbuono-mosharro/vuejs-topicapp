e2977a82cb40935e3c3d146105ea8b1bf566c643
Marinario <ilbuonomosharro@gmail.com>
2023-07-18 06:50:21 +0200

    Updated router/index.js added reuiredAuth

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import Home from "../components/pages/Home.vue";
 import Topic from "../components/topics/Topic.vue";
 import Login from "../components/auth/Login.vue";
@@ -6,20 +6,29 @@ import SignUp from "../components/accounts/SignUp.vue";
 import AddTopic from "../components/topics/AddTopic.vue";
 import UpdateTopic from "../components/topics/UpdateTopic.vue";
 import UserProfile from "../components/accounts/UserProfile.vue";
+import {useAuthStore} from "../stores/authStore.js";
 
 const routes = [
-  { path: '/', component: Home, name:"home" },
-  { path: '/topic/add', component: AddTopic, name: "addTopic" },
-  { path: '/topic/:id', component: Topic, name: "topicDetail" },
-    { path: '/topic/update/:id', component: UpdateTopic, name: "topicUpdate" },
-  { path: '/login', component: Login, name:"login" },
-  { path: '/sign-up', component: SignUp, name:"signup" },
-  {path: '/profile/:id', component: UserProfile, name: "userProfile"},
+    {path: '/', component: Home, name: "home"},
+    {path: '/topic/add', component: AddTopic, name: "addTopic", meta: { requiresAuth: true },},
+    {path: '/topic/:id', component: Topic, name: "topicDetail"},
+    {path: '/topic/update/:id', component: UpdateTopic, name: "topicUpdate", meta: { requiresAuth: true },},
+    {path: '/login', component: Login, name: "login"},
+    {path: '/sign-up', component: SignUp, name: "signup"},
+    {path: '/profile/:id', component: UserProfile, name: "userProfile", meta: { requiresAuth: true },},
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes, // short for `routes: routes`
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+})
+
+router.beforeEach((to) => {
+    // ✅ This will work because the router starts its navigation after
+    // the router is installed and pinia will be installed too
+    const store = useAuthStore()
+
+    if (to.meta.requiresAuth && !store.isAuthenticated) return '/login'
 })
 
 export default router
