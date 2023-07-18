a37ae452a7e6b6a2d3944e95146358d122079231
Marinario <ilbuonomosharro@gmail.com>
2023-07-18 06:02:08 +0200

    refactor the categoriesStore store, added axio for requests

--- src/stores/categoriesStore.js
+++ src/stores/categoriesStore.js
@@ -1,26 +1,21 @@
 import {defineStore} from 'pinia'
+import axios from "axios";
 const base_url = "http://127.0.0.1:8000/api"
 
 export const useCategoryStore = defineStore('category', {
     state: () => (
         {
             data: null,
-            success: null,
-            failed: null,
             error: null,
             loading: true,
         }
     ),
     actions: {
         async fetchCategories() {
-             try {
-                const response = await fetch(`${base_url}/categories/`)
-                if (response.ok) {
-                    this.data = await response.json()
-                    this.success = true
-                } else {
-                    this.failed = true
-                }
+            this.loading = true
+            try {
+                const response = await axios.get(`${base_url}/categories/`)
+                this.data = response.data
             } catch (e) {
                 this.error = e.message
             } finally {
